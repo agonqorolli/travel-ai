8206b56c64b2bdbec3f0bd10da5e034923df25c4
Agon Qorolli <agon.qorolli@trustedfamily.net>
2023-05-25 23:40:44 +0200

    add prototype

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  Button,
+  Select,
+  Stack,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  InputRightElement,
+  Heading,
+} from "@chakra-ui/react";
+import { CalendarIcon, CheckIcon } from "@chakra-ui/icons";
 
 function App() {
+  async function postJSON(data) {
+    try {
+      const response = await fetch(
+        "https://d6sa2alo0j.execute-api.us-east-1.amazonaws.com/default/testRequest",
+        {
+          method: "POST",
+          mode: "no-cors",
+          headers: {
+            "Content-type": "application/json",
+            // "Access-Control-Allow-Origin": "*",
+            // "Access-Control-Allow-Methods": "POST",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      // const result = await response.json();
+      console.log("Success:", response);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  function handleGenerateTravelPlan() {
+    postJSON({
+      prompt: "Tell me a joke about dogs",
+    })
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((er) => {
+        console.log("er", er);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container my={8}>
+      <Heading>Travel Planner</Heading>
+
+      <Stack mt={8}>
+        <Select placeholder="Select Country">
+          <option value="SP">Spain</option>
+          <option value="IT">Italy</option>
+          <option value="BR">Brazil</option>
+        </Select>
+
+        <Select placeholder="Select City">
+          <option value="B">Barcelona</option>
+          <option value="M">Milan</option>
+          <option value="R">Rio</option>
+        </Select>
+
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            color="gray.300"
+            fontSize="1.2em"
+            children="$"
+          />
+          <Input type="number" placeholder="Enter budget" />
+          <InputRightElement>
+            <CheckIcon color="green.500" />
+          </InputRightElement>
+        </InputGroup>
+
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            color="gray.300"
+            fontSize="1.2em"
+          >
+            <CalendarIcon color="gray.300" />
+          </InputLeftElement>
+          <Input type="number" placeholder="Enter days" />
+          <InputRightElement>
+            <CheckIcon color="green.500" />
+          </InputRightElement>
+        </InputGroup>
+
+        <Button onClick={handleGenerateTravelPlan}>Generate travel plan</Button>
+      </Stack>
+    </Container>
   );
 }
 
